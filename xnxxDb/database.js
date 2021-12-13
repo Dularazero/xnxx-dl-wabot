9c721d762717d7272bae2ba3c141898cf6d01e49
✬Ravindu Manoj✬ ✓ <88134633+ravindu01manoj@users.noreply.github.com>
2021-12-13 10:00:03 +0530

    Create database.js

---
+++ xnxxDb/database.js
@@ -0,0 +1,39 @@
+/* Codded by @Ravindu Manoj
+
+Telegram: t.me/RavinduManoj
+Facebook: https://www.facebook.com/ravindu.manoj.79
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+Whats bot - Ravindu Manoj
+*/
+
+
+let Detkails = require('xnxx-dl-wa');
+let Details = Detkails.config
+const { DataTypes } = require('sequelize');
+
+const PluginDB = Details.DATABASE.define('Plugin', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    url: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    }
+});
+
+async function installPlugin(adres, file) {
+    var Plugin = await PluginDB.findAll({
+        where: {url: adres}
+    });
+
+    if (Plugin.length >= 1) {
+        return false;
+    } else {
+        return await PluginDB.create({ url: adres, name: file });
+    }
+}
+module.exports = { PluginDB: PluginDB, installPlugin: installPlugin };
